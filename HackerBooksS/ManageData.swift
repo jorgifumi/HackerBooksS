0ffffd9db47767134f9db9ee28952494af3156a8
Jorge Lucena <jorgifumi@gmail.com>
2016-01-05 18:15:25 +0100

    Primera version downloadJSON

--- HackerBooksS/ManageData.swift
+++ HackerBooksS/ManageData.swift
@@ -11,57 +11,72 @@ import UIKit
 
 func downloadJSON(){
     
-    let previousDataKey = "madreMiaConLaPractica"
+    let firstRun = "madreMiaConLaPractica"
     
     //comprobar si hay datos locales
     
-    //NSUserDefaults
     let ud = NSUserDefaults.standardUserDefaults()
-    var url = NSURL(string: "https://t.co/K9ziV0z3SJ")
+        let isFirstRun = ud.objectForKey(firstRun)
     
-    if ud.objectForKey(previousDataKey) !== nil {
-        // Ya existen datos, poner ruta local
+    if isFirstRun == nil {
+        // Primera vez que se arranca
+        let url = NSURL(string: "https://t.co/K9ziV0z3SJ")
         let fm = NSFileManager.defaultManager()
-    
-        if let tempUrl = fm.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first?.URLByAppendingPathComponent("books_readable.json") {
-            url = tempUrl
-        }
-        print("Error al crear la URL")
-      
-    }
-    
-    //Leer Datos
-    //let data = data_request(url)
-    
-    do{
-        try "prueba".writeToURL(url!, atomically: true, encoding: NSUTF8StringEncoding)
-        var str:AnyObject?
-        
-        downloadFile(fromURL: url!, toURL: <#T##String#>)
-    }catch{
-        print("Error al guardar los datos")
-        
-    }
-    
-    
-    
 
-}
-
-
-func downloadFile(fromURL url: NSURL, toURL: String) {
-    
-    let task = NSURLSession.sharedSession().dataTaskWithURL( url, completionHandler: {
-        (data, response, error) -> Void in
-        dispatch_async(dispatch_get_main_queue()) {
-            if let data = data {
-                
-                data.writeToFile(toURL, atomically: true)
-                
+        // Descargar JSON
+        if let data = NSData(contentsOfURL: url!) {
+            // Guardarlo en Documents
+            if let dataUrl = fm.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first?.URLByAppendingPathComponent("books_readable.json") {
+                data.writeToURL(dataUrl, atomically: true)
+            
             }
+            isFirstRun?.setDefaults()
         }
-    })
-    task.resume()
-
+        
+        
+        
+    }
 }
+//    else{
+//        // Ya existen datos, poner ruta local
+//        
+//    
+//        if let tempUrl = fm.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first?.URLByAppendingPathComponent("books_readable.json") {
+//            url = tempUrl
+//        }
+//        print("Error al crear la URL")
+//      
+//    }
+//    
+//    //Leer Datos
+//    //let data = data_request(url)
+//    
+//    do{
+//        try "prueba".writeToURL(url!, atomically: true, encoding: NSUTF8StringEncoding)
+//        var str:AnyObject?
+//        
+//        downloadFile(fromURL: url!, toURL: <#T##String#>)
+//    }catch{
+//        print("Error al guardar los datos")
+//        
+//    }
+    
+
+//
+//
+//func downloadFile(fromURL url: NSURL, toURL: String) {
+//    
+//    let task = NSURLSession.sharedSession().dataTaskWithURL( url, completionHandler: {
+//        (data, response, error) -> Void in
+//        dispatch_async(dispatch_get_main_queue()) {
+//            if let data = data {
+//                
+//                data.writeToFile(toURL, atomically: true)
+//                
+//            }
+//        }
+//    })
+//    task.resume()
+//
+//}
 
